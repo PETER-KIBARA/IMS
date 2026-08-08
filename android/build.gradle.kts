c8541f29b8f8390c126404f8a0a02dcb247b8959
PETER-KIBARA <peterkibara1155@gmail.com>
2026-08-08 20:57:29 +0300

    Inject fallback namespace for legacy plugins and upgrade AGP to 8.7.3

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -13,6 +13,16 @@ subprojects {
     project.evaluationDependsOn(":app")
 }
 
+// Auto-inject missing namespace for legacy dependencies (like escposprinter)
+subprojects {
+    afterEvaluate {
+        val android = project.extensions.findByName("android") as? com.android.build.gradle.BaseExtension
+        if (android != null && android.namespace == null) {
+            android.namespace = "com.legacy.${project.name.replace("-", "_")}"
+        }
+    }
+}
+
 tasks.register<Delete>("clean") {
     delete(rootProject.buildDir)
 }
